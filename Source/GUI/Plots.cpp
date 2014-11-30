13cee3ea98cf667c681647c4c00ff6973c6098a6
Jérôme Martinez <jerome@mediaarea.net>
2014-11-30 14:37:08 +0100

    Workaround because 00:00:00 puts the axis too much on the right. There is still a problem with other values.

--- Source/GUI/Plots.cpp
+++ Source/GUI/Plots.cpp
@@ -83,7 +83,7 @@ private:
                 QString label;
 
                 if ( value == 0 )
-                    label = "0"; // Workaround because 00:00:00 puts the axis too much on the right. There is still a problem with other values
+                    label = "0"; // Workaround because 00:00:00 puts the axis too much on the right. There is still a problem with other values.
                 else if ( scaleDiv().interval().width() > 10.0 )
                 {
                     label.sprintf( "%02d:%02d:%02d", 
