f30d173adbfc7a7075b60ad5dba0397469ab844e
beholder <elderorb@gmail.com>
2017-02-28 00:51:13 +0300

    disable Qt's debug output

--- Source/Cli/main.cpp
+++ Source/Cli/main.cpp
@@ -13,8 +13,17 @@ enum Errors {
     InvalidInput = 4
 };
 
+// suppress debug output
+void messageHandler(QtMsgType type, const QMessageLogContext &context, const QString &msg)
+{
+    Q_UNUSED(type);
+    Q_UNUSED(context);
+    Q_UNUSED(msg);
+}
+
 int main(int argc, char *argv[])
 {
+    qInstallMessageHandler(messageHandler);
     QCoreApplication a(argc, argv);
 
     QString input;
