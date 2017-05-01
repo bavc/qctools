02a2cdc9e803ff6e8d5c5c7fceea0fa2a7d9c538
Dave Rice <dave@dericed.com>
2017-05-01 11:30:48 -0400

    updating decklink sdk mirror
    per
    http://www.linuxquestions.org/questions/showthread.php?s=e43d6cf2f0a79c9
    22daed51efcd92c47&p=5522644#post5522644

--- Project/BuildAllFromSource/init_blackmagic.sh
+++ Project/BuildAllFromSource/init_blackmagic.sh
@@ -3,7 +3,7 @@
 echo "PWD: " + $PWD
 
 if [ ! -d "Blackmagic DeckLink SDK" ] ; then
-    wget http://slackware.uk/sbosrcarch/by-name/multimedia/vlc/Blackmagic_DeckLink_SDK_10.1.4.zip
+    wget http://ponce.cc/slackware/sources/repo/Blackmagic_DeckLink_SDK_10.1.4.zip
     unzip Blackmagic_DeckLink_SDK_10.1.4.zip
     mv "Blackmagic DeckLink SDK 10.1.4" "Blackmagic DeckLink SDK"
 fi
