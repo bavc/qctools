0d192dd385fdd6d45833c58eb2cf9583f74e7c84
Maxime Gervais <gervais.maxime@gmail.com>
2020-01-28 14:26:41 +0100

    Fix crash in qcli if the first stream isn't of video or audio type
    Signed-off-by: Maxime Gervais <gervais.maxime@gmail.com>

--- Source/Cli/cli.cpp
+++ Source/Cli/cli.cpp
@@ -286,13 +286,10 @@ int Cli::exec(QCoreApplication &a)
         return InvalidInput;
     }
 
-    std::vector<size_t> framesCountForAllStreams = info->Glue->FramesCountForAllStreams();
-    size_t indexOfStreamWithKnownTotal = 0;
-
-    for(size_t i = 0; i < framesCountForAllStreams.size(); ++i)
+    for(int i = 0; i < info->Glue->StreamCount_Get(); ++i)
     {
-        if(framesCountForAllStreams[i] > framesCountForAllStreams[indexOfStreamWithKnownTotal])
-            indexOfStreamWithKnownTotal = i;
+        if(info->Glue->FramesCountPerStream(i) > info->Glue->FramesCountPerStream(indexOfStreamWithKnownFrameCount))
+            indexOfStreamWithKnownFrameCount = i;
     }
 
     if(!info->hasStats() || forceOutput)
