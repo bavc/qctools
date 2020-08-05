4631f2917febfc8a622d5f26ea700fd384b69914
Alexander Ivash <elderorb@gmail.com>
2020-08-05 19:14:21 +0300

    switch to forked qwt 6.1.5

--- Project/BuildAllFromSource/init_qwt.sh
+++ Project/BuildAllFromSource/init_qwt.sh
@@ -3,48 +3,10 @@
 echo "PWD: " + $PWD
 
 if [ ! -d qwt ] ; then
-    wget https://github.com/osakared/qwt/archive/tags/qwt-6.1.2.zip
-    unzip qwt-6.1.2.zip
-    mv qwt-tags-qwt-6.1.2 qwt
-    rm qwt-6.1.2.zip
-    patch qwt/qwtconfig.pri <<EOF
---- qwtconfig.pri	2016-01-03 15:41:26.000000000 -0500
-+++ qwtconfig.pri copy	2016-01-03 15:41:26.000000000 -0500
-@@ -72,7 +72,7 @@
- # it will be a static library.
- ######################################################################
-
--QWT_CONFIG           += QwtDll
-+#QWT_CONFIG           += QwtDll
-
- ######################################################################
- # QwtPlot enables all classes, that are needed to use the QwtPlot 
-@@ -93,13 +93,13 @@
- # export a plot to a SVG document
- ######################################################################
-
--QWT_CONFIG     += QwtSvg
-+#QWT_CONFIG     += QwtSvg
-
- ######################################################################
- # If you want to use a OpenGL plot canvas
- ######################################################################
-
--QWT_CONFIG     += QwtOpenGL
-+#QWT_CONFIG     += QwtOpenGL
-
- ######################################################################
- # You can use the MathML renderer of the Qt solutions package to 
-@@ -118,7 +118,7 @@
- # Otherwise you have to build it from the designer directory.
- ######################################################################
-
--QWT_CONFIG     += QwtDesigner
-+#QWT_CONFIG     += QwtDesigner
-
- ######################################################################
- # Compile all Qwt classes into the designer plugin instead
-EOF
+    wget https://github.com/ElderOrb/qwt/archive/v6.1.5.zip
+    unzip v6.1.5.zip
+    mv qwt-6.1.5 qwt
+    rm v6.1.5.zip
 fi
 cd qwt
 $BINQMAKE
