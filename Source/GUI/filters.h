fd4f78e1917ab7d90c0225604106bf5a07d801ce
Dave Rice <dave@dericed.com>
2020-10-09 11:06:49 -0400

    force pixscope output to rgb24

--- Source/GUI/filters.h
+++ Source/GUI/filters.h
@@ -254,8 +254,8 @@ const filter Filters[] =
             { Args_Type_None,        0,   0,       0,   0, nullptr },
         },
         {
-            "pixscope=x=${2}/${width}:y=${3}/${height}:w=${4}:h=${5}",
-            "il=l=d:c=d,pixscope=x=${2}/${width}:y=${3}/${height}:w=${4}:h=${5}",
+            "pixscope=x=${2}/${width}:y=${3}/${height}:w=${4}:h=${5},format=rgb24",
+            "il=l=d:c=d,pixscope=x=${2}/${width}:y=${3}/${height}:w=${4}:h=${5},format=rgb24",
         },
     },
     {
