b711f647cfb091f66f158001a0313abe145a1c08
Alexander Ivash <elderorb@gmail.com>
2023-08-26 19:14:47 +0200

    introduce logging capabilities into qcli

--- Source/Cli/cli.cpp
+++ Source/Cli/cli.cpp
@@ -4,6 +4,7 @@
 #include "Core/CommonStats.h"
 #include "Core/FileInformation.h"
 #include <QDir>
+#include <Core/logging.h>
 
 Cli::Cli() : indexOfStreamWithKnownFrameCount(0), statsFileBytesWritten(0), statsFileBytesTotal(0), statsFileBytesUploaded(0), statsFileBytesToUpload(0)
 {
@@ -15,6 +16,7 @@ int Cli::exec(QCoreApplication &a)
     std::string appName = "qcli";
     std::string copyright = "Copyright (C): 2013-2020, BAVC.\nCopyright (C): 2018-2020, RiceCapades LLC & MediaArea.net SARL.";
     Preferences prefs;
+    Logging logging;
 
     QString input;
     QString output;
@@ -53,6 +55,9 @@ int Cli::exec(QCoreApplication &a)
         } else if(a.arguments().at(i) == "-u")
         {
             uploadToSignalServer = true;
+        } else if(a.arguments().at(i) == "--log")
+        {
+            logging.enable();
         } else if(a.arguments().at(i) == "-uf")
         {
             forceUploadToSignalServer = true;
